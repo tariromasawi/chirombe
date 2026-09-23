c2c68725f53d701225f53b619ad5e5f02385d1bc
MWARINDIMWARI <tariromasawi19@gmail.com>
2026-09-23 11:00:13 +0100

    Liturgy attach loads seed embed before composer.

--- js/liturgy-attach.js
+++ js/liturgy-attach.js
@@ -1,5 +1,6 @@
 (function(){
   function add(src){var s=document.createElement("script");s.src=src;document.body.appendChild(s)}
+  add("./js/liturgy-seeds-embed.js");
   add("./js/prayer-composer.js");
   add("./js/resonance-engine.js");
   add("./js/liturgy-matrix.js");
